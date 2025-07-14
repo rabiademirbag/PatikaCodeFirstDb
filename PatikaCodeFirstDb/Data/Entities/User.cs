88190032270bee01d19147180a372499831608d2
rabiademirbag <rddemirbagg@gmail.com>
2025-07-15 00:12:39 +0300

    Proje dosyası ekle.

---
+++ PatikaCodeFirstDb/Data/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace PatikaCodeFirstDb.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        //bir kullanıcının birden fazla postu olabilir
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ PatikaCodeFirstDb/Data/PatikaSecondDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatikaCodeFirstDb.Data.Entities;
+
+namespace PatikaCodeFirstDb.Data
+{
+    public class PatikaSecondDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            @"Server=.\SQLEXPRESS;Database=PatikaCodeFirstDb2;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False"
+            );
+        }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasOne(p=>p.User).WithMany(u => u.Posts)
+                .HasForeignKey(p => p.UserId);
+        }
+    }
+    }
+
